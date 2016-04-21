62952747075d2817bdcae618700b0acfaa04c9b7
Alex Nguyen <me@alexmnguyen.com>
2016-04-20 23:40:26 -0400

    initial commit

---
+++ data/database.js
@@ -0,0 +1,46 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+// Model types
+class User {}
+class Widget {}
+
+// Mock data: create 2 users
+var viewer = new User();
+var viewer2 = new User();
+
+viewer.id = '1';
+viewer.name = 'Anonymous';
+var widgets = ['What\'s-it', 'Who\'s-it', 'How\'s-it'].map((name, i) => {
+  var widget = new Widget();
+  widget.name = name;
+  widget.id = `${i}`;
+  widget.viewerId = '1';
+  return widget;
+});
+
+function getWidgetsByViewer(id){
+  var viewerWidgets = []
+
+  widgets.forEach(function(widget){
+    if(widget.viewerId === id){
+      return widget;
+    }
+  })
+}
+
+module.exports = {
+  // Export methods that your schema can use to interact with your database
+  getUser: (id) => id === viewer.id ? viewer : null,
+  getViewer: () => viewer,
+  getWidget: (id) => widgets.find(w => w.id === id),
+  getWidgets: () => widgets,
+  User,
+  Widget,
+};
